75db75b06bc4671b16e8d774cb1194d9d3c34f6d
unknown <fozlurrahman9933@gmail.com>
2023-08-23 20:25:29 +0600

    final output the merge two array sort

--- merge-two-sort-array.cpp
+++ merge-two-sort-array.cpp
@@ -1,25 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void merge(int a[], int l, int m, int r)
 {
-    int leftSize = l - m + 1;
+    int leftSize = m - l + 1;
     int rightSize = r - m;
     int lArray[leftSize];
     int rArray[rightSize];
-    int k = 0;
 
-    for (int i = l; i <= m; i++)
+    for (int i = 0; i < leftSize; i++)
     {
-        lArray[k] = a[i];
-        k++;
+        lArray[i] = a[l + i];
     }
-    k = 0;
-    for (int i = m + 1; i <= r; i++)
+    for (int i = 0; i < rightSize; i++)
     {
-        rArray[k] = a[i];
-        k++;
+        rArray[i] = a[m + 1 + i];
+    }
+
+    int i = 0, j = 0;
+    int cur = l;
+
+    while (i < leftSize && j < rightSize)
+    {
+        if (lArray[i] <= rArray[j])
+        {
+            a[cur] = lArray[i];
+            i++;
+        }
+        else
+        {
+            a[cur] = rArray[j];
+            j++;
+        }
+        cur++;
+    }
+
+    while (i < leftSize)
+    {
+        a[cur] = lArray[i];
+        i++;
+        cur++;
+    }
+
+    while (j < rightSize)
+    {
+        a[cur] = rArray[j];
+        j++;
+        cur++;
+    }
+
+    for (int i = 0; i <= r; i++)
+    {
+        cout << a[i] << " ";
     }
 }
+
 int main()
 {
     int n;
